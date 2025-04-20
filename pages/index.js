8ceeda2ec35bf443957d5b634c67409ff147b027
Varun Anand Patkar <varunpatkar501@gmail.com>
2025-04-20 20:31:38 +0530

    Added favicons

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,27 @@ export default function Home() {
 			<Head>
 				<title>Your Multiverse Vault</title>
 				<meta name="description" content="Virtual Art Gallery Experience" />
-				<link rel="icon" href="/favicon.ico" /> {/* Optional: Add a favicon */}
+				{/* Favicon Links */}
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				{/* Add the standard favicon.ico link */}
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
 				<Scene />
